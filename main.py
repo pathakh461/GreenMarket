6a1cbf46ae383d863582010fc2712ac234674bb1
Himanshu Pathak <himanshupathak@Himanshus-MacBook-Air.local>
2024-01-14 15:58:00 +0530

    changed main

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from application.database import db
 
+
 def create_app():
-    app =Flask(__name__)
+    app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.secret_key = "secret"
     db.init_app(app)
-    
 
     app.app_context().push()
 
     return app
+
+
 app = create_app()
 
 from application.controllers import *
@@ -19,4 +21,4 @@ from application.controllers import *
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(host="0.0.0.0", port=2345, debug=True)
+    app.run(host="0.0.0.0", debug=False)
